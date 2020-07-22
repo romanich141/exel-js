d6de057a2969b678a0f7f5bcbb4a54537d7196d3
romanich141 <romaovcharenko18011993@gmail.com>
2020-07-22 19:27:41 +0300

    config webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('connect webpack');
+import './scss/index.scss';
+
+
+async function getRandomNumber() {
+    await new Promise(resolve => {
+        return resolve(111);
+    })
+};
+
+getRandomNumber();
+
