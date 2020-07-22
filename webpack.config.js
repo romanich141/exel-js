bd3ec2eda2cd2cb09ea2d815370efcfd2b4e1b24
romanich141 <romaovcharenko18011993@gmail.com>
2020-07-22 20:07:01 +0300

    add eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
-console.log(isDev)
-const fileName = ext => isProd ? `bundle.${ext}` : `bundle.[hash].${ext}`
+const fileName = ext => isProd ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+const jsLoaders = () => {
+    let loaders = [
+        {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            },
+        },
+    ];
+    if (isDev) {
+        loaders.push('eslint-loader');
+    }
+    return loaders;
+};
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
@@ -23,9 +37,9 @@ module.exports = {
     resolve: {
         extensions: ['.js'],
         alias: {
-            "@": path.resolve(__dirname, 'src'),
-            "@core": path.resolve(__dirname, 'src/core'),
-        }
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core'),
+        },
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -36,14 +50,14 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-                { 
-                    from: path.resolve(__dirname, 'src/favicon.svg'), 
-                    to: path.resolve(__dirname, 'dist'), 
+                {
+                    from: path.resolve(__dirname, 'src/favicon.svg'),
+                    to: path.resolve(__dirname, 'dist'),
                 },
               ],
         }),
         new MiniCssExtractPlugin({
-            filename: fileName('css')
+            filename: fileName('css'),
         }),
     ],
     module: {
@@ -56,7 +70,7 @@ module.exports = {
                     options: {
                         hmr: isDev,
                         reloadAll: true,
-                    }
+                    },
                 },
                 'css-loader',
                 'sass-loader',
@@ -65,13 +79,8 @@ module.exports = {
           {
             test: /\.m?js$/,
             exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }
+            use: jsLoaders(),
+          },
         ],
     },
-}
+};
